8319338fcde852e82c8a4f0d5f82c6537a3c0cf0
EngYOSHI <243426029@ccmailg.meijo-u.ac.jp>
2025-07-06 17:46:45 +0900

    PEP8に準拠

--- main.py
+++ main.py
@@ -11,40 +11,54 @@ import time
 CSV_URL = "https://www.vpngate.net/api/iphone/"
 DEBUG = True
 
+
 def main():
     print_debug("Started.")
-    server_list = get_server_list() # VPNGateのサーバ情報を取得
+    server_list = get_server_list()  # VPNGateのサーバ情報を取得
 
 
-def get_server_list(country:str = None, port:int = None):
-    l = []
+def get_server_list(country: str = None, port: int = None):
+    res = []
     with requests.Session() as s:
         print_debug("Getting VPNGate server list csv.")
         while True:
             try:
-                content = s.get(CSV_URL).content.decode('utf-8')
-                break # contentをループ外で使うため
+                content = s.get(CSV_URL).content.decode("utf-8")
+                break  # contentをループ外で使うため
             except Exception as e:
                 print_error("GetServerListCSV", e)
                 time.sleep(3)
                 continue
-        server_list = list(csv.reader(StringIO(content), delimiter=','))
-        # [0]HostName,[1]IP,[2]Score,[3]Ping,[4]Speed,[5]CountryLong,[6]CountryShort,[7]NumVpnSessions,[8]Uptime,
-        # [9]TotalUsers,[10]TotalTraffic,[11]LogType,[12]Operator,[13]Message,[14]OpenVPN_ConfigData_Base64
-        server_list = server_list[2:-1] # 1,2行目と最終行は不要な情報
+        server_list = list(csv.reader(StringIO(content), delimiter=","))
+        # [0]HostName,[1]IP,[2]Score,[3]Ping,[4]Speed,
+        # [5]CountryLong,[6]CountryShort,[7]NumVpnSessions,[8]Uptime,
+        # [9]TotalUsers,[10]TotalTraffic,[11]LogType,[12]Operator,
+        # [13]Message,[14]OpenVPN_ConfigData_Base64
+        server_list = server_list[2:-1]  # 1,2行目と最終行は不要な情報
         print_debug("▼ServerList")
         for s in server_list:
-            # hostname, ip, port, score, ping, speed, country, num_vpn_sessions, uptime, operator
-            sinfo = ServerConnectInfo(s[0], s[1], get_port_from_openvpn(s[14]), s[2], s[3], s[4],
-                                      s[6], s[7], s[8], s[12])
+            # hostname, ip, port, score, ping, speed,
+            # country, num_vpn_sessions, uptime, operator
+            sinfo = ServerConnectInfo(
+                s[0],
+                s[1],
+                get_port_from_openvpn(s[14]),
+                s[2],
+                s[3],
+                s[4],
+                s[6],
+                s[7],
+                s[8],
+                s[12],
+            )
             if country is not None and sinfo.country != country:
                 continue
             if port is not None and sinfo.port != port:
                 continue
-            l.append(sinfo)
+            res.append(sinfo)
             print_debug(repr(sinfo), False)
-        l.sort(key=lambda x: x.score)
-        return l
+        res.sort(key=lambda x: x.score)
+        return res
 
 
 def get_port_from_openvpn(base64str):
@@ -54,13 +68,13 @@ def get_port_from_openvpn(base64str):
 
     Args:
         base64str (string): base64文字列
-    
+
     Returns:
         int: TCPポート番号
     """
     s = base64.b64decode(base64str).decode()
     if "proto tcp" in s:
-        match = re.search(r'remote \d{1,3}(?:\.\d{1,3}){3} (\d+)', s)
+        match = re.search(r"remote \d{1,3}(?:\.\d{1,3}){3} (\d+)", s)
         if match:
             port = int(match.group(1))
             return port
@@ -69,8 +83,19 @@ def get_port_from_openvpn(base64str):
 
 
 class ServerConnectInfo:
-    def __init__(self, hostname, ip, port, score, ping, speed,
-                 country, num_vpn_sessions, uptime, operator):
+    def __init__(
+        self,
+        hostname,
+        ip,
+        port,
+        score,
+        ping,
+        speed,
+        country,
+        num_vpn_sessions,
+        uptime,
+        operator,
+    ):
         self.hostname = hostname
         self.ip = ip
         self.port = port
@@ -81,7 +106,7 @@ class ServerConnectInfo:
         self.num_vpn_sessions = num_vpn_sessions
         self.uptime = uptime
         self.operator = operator
-    
+
     def get_speed(self):
         unit = ["B", "KB", "MB", "GB", "TB", "PB"]
         index_unit = 0
@@ -98,7 +123,7 @@ class ServerConnectInfo:
         return f"{self.hostname}: {self.ip}:{self.port} ({self.country}) Score:{self.score} Ping:{self.ping}ms {self.get_speed()}"
 
 
-def print_debug(msg, banner=True, end='\n'):
+def print_debug(msg, banner=True, end="\n"):
     if DEBUG:
         if banner:
             print("\033[45m(DEBUG)\033[0m " + str(msg), end=end)
@@ -110,6 +135,6 @@ def print_error(errtype, errmsg):
     print("\033[31m" + str(errtype) + ": " + str(errmsg) + "\033[0m")
 
 
-if __name__=="__main__":
-    os.system("") # Windowsにて、色付き文字を出力するためのおまじない
+if __name__ == "__main__":
+    os.system("")  # Windowsにて、色付き文字を出力するためのおまじない
     main()
