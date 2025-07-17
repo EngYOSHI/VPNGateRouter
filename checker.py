01cc2a9abd74ff63a9a75c2ce88a6847536de302
EngYOSHI <243426029@ccmailg.meijo-u.ac.jp>
2025-07-18 02:36:43 +0900

    Change: checker.py
    ・記録する日時を，記録時点ではなく処理開始時点とした

--- checker.py
+++ checker.py
@@ -28,30 +28,30 @@ def web_con_worker():
 
 
 def web_con(url: str) -> bool:
+    dt = datetime.now(ZoneInfo("Asia/Tokyo"))
     try:
         start = time.perf_counter()
-        res = requests.get(url, timeout=3)
+        res = requests.get(url, timeout=INTERVAL)
         end = time.perf_counter()
     except Exception as ex:
         # エラー
-        log_write("web", 1, ex)
+        log_write(dt, "web", 1, ex)
     else:
         if res.status_code == 200:
             match = re.search(r"^ip=([^\s]+)", res.text, re.MULTILINE)
             if match:
                 ms = (end - start) * 1000
                 ip = match.group(1)
-                log_write("web", 0, f"{ip}; {ms:.3f}")
+                log_write(dt, "web", 0, f"{ip}; {ms:.3f}")
             else:
                 # データのパースに失敗．ほぼ起こり得ないはず．
-                log_write("web", 2, "Parse error")
+                log_write(dt, "web", 2, "Parse error")
         else:
             # ステータスコードが200でない．サーバサイドの問題で起こり得る
-            log_write("web", 3, res.status_code)
+            log_write(dt, "web", 3, res.status_code)
 
 
-def log_write(type: str, code: int, msg: str):
-    dt = datetime.now(ZoneInfo("Asia/Tokyo"))
+def log_write(dt, type: str, code: int, msg: str):
     path = Path(__file__).resolve().parent.joinpath(f"check_log/{type}-{dt.date()}.txt")
     os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path, mode="a", encoding="utf-8", newline='\n') as f:
